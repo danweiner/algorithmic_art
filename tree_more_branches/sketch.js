375b08be455c30f4176f9e4e22b6881722066090
Dan Weiner <daweiner16@gmail.com>
2018-07-02 13:00:20 -0400

    move strokeWeight to tree function, increase length to 110

--- tree_more_branches/sketch.js
+++ tree_more_branches/sketch.js
@@ -9,12 +9,12 @@ function draw() {
 	noFill(); stroke(0); rect(0, 0, 799, 599);
 	stroke(150, 100, 0, 100);
 	// strokeWeight decreased to 2 from 4
-	strokeWeight(2);
+	
 
 	// start tree at (400, 500)
 	// pointing straight up with a stated depth
 
-	tree(400, 500, 0, 6); 
+	tree(400, 500, 5, 6); 
 }
 
 // tree function takes coordinates of tree, angle, and current depth
@@ -23,7 +23,7 @@ function tree(x1, y1, angle, depth) {
 
 	// length of branch
 	// using tanh/4, for depths of 5 or below, function falls smoothly
-	var length = 100 * Math.tanh(depth/4);
+	var length = 110 * Math.tanh(depth/4);
 
 	// use angle to calculate endpoint of branch
 	var x2 = x1 - (length * sin(angle));
@@ -33,6 +33,7 @@ function tree(x1, y1, angle, depth) {
 	var x4 = x1 - (0.4 * length * sin(angle));
 	var y4 = y1 - (0.4 * length * cos(angle));
 
+	strokeWeight(0.2 + depth/2);
 	// this draws the tree trunk - straight up, angle starts at 0
 	line(x1, y1, x2, y2);
 
