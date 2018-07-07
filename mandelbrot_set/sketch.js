d3e99eef38946760a7a77c48a0aadc2a6beca658
Dan Weiner <daweiner16@gmail.com>
2018-07-07 16:12:53 -0400

    add border, zoom and shift fractal

--- mandelbrot_set/sketch.js
+++ mandelbrot_set/sketch.js
@@ -5,13 +5,14 @@ function setup() {
 }
 
 function draw() {
+	noFill(); stroke(0); rect(0, 0, 599, 599);
 	// visit every pixel on the canvas
 	for (var x = 0; x < 600; x++) {
 		for (var y = 0; y < 600; y++) {
 
 			// convert canvas (x, y) to complex number c = a + bi
-			var a = map(x, 0, 599, -2, 2);
-			var b = map(y, 0, 599, 2, -2);
+			var a = map(x, 0, 599, -2.25, 0.75);
+			var b = map(y, 0, 599, 1.5, -1.5);
 
 			// start with z=0+0i and c=a+bi
 			// iterate 50 times or until |z|>2
